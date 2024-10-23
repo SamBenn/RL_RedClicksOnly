8c256ca8d7f3e1f0c5d93223da40b274a23d58ad
Sam Benn <sam-benn@hotmail.co.uk>
2024-10-23 22:02:54 +0100

    Implemented

---
+++ src/main/java/com/MyBulwark/RedClicksOnly/RedClicksOnlyPlugin.java
@@ -0,0 +1,24 @@
+package com.MyBulwark.RedClicksOnly;
+
+import javax.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.Client;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.api.events.MenuOptionClicked;
+
+@Slf4j
+@PluginDescriptor(
+	name = "Red Clicks Only"
+)
+public class RedClicksOnlyPlugin extends Plugin
+{
+	@Subscribe
+	public void onMenuOptionClicked(MenuOptionClicked event)
+	{
+		if (event.getMenuOption().equalsIgnoreCase("Walk here")) {
+			event.consume();
+		}
+	}
+}
--- src/test/java/com/MyBulwark/RedClicksOnly/RedClicksOnlyTest.java
+++ src/test/java/com/MyBulwark/RedClicksOnly/RedClicksOnlyTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.MyBulwark.RedClicksOnly;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class RedClicksOnlyTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(RedClicksOnlyPlugin.class);
 		RuneLite.main(args);
 	}
 }
